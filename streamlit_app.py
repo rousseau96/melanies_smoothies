ceab6afdf81073a6aba08e7033a4ffeadd58fc0c
rousseau96 <50132358+rousseau96@users.noreply.github.com>
2024-11-25 16:28:23 +0100

    Update streamlit_app.py
    Rearrangement of the code to have 2 lines put in the fruit chosen loop

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 # Import python packages
 import streamlit as st
-# from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col
+import requests
 
 # Write directly to the app
 st.title(":cup_with_straw: Customize Your Smoothie :cup_with_straw:")
@@ -28,6 +28,8 @@ if ingredients_list :
 
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+        st_df = st.dataframe(data = smoothiefroot_response.json(), use_container_width = True) 
 
     #st.write(ingredients_string)
 
@@ -45,7 +47,4 @@ if ingredients_list :
 
         st.success('Your Smoothie is ordered, ' + name_on_order + '!', icon="✅")
 
-import requests
-smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-# st.text((smoothiefroot_response).json())
-st_df = st.dataframe(data = smoothiefroot_response.json(), use_container_width = True) 
+
